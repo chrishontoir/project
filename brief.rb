937b8134a33313d01c767e2b1eee1fdf26525353
user <user@users-MacBook-Pro.local>
2019-01-18 17:17:45 +0000

    Added a brief document.

---
+++ brief.rb
@@ -0,0 +1,17 @@
+# BRIEF
+#
+# Create a working card game that allows players to register, edit and delete accounts, create decks of cards,
+# and play against other players.
+#
+# MVP
+# - Create an assortment of cards that can be used by the players in their decks.
+# - Create, edit and delete players.
+# - Create, and delete decks.
+# - Create, update and delete games.
+# - Have working game logic that allows two players to play vs each other.
+#
+# Extensions
+# - Implement a win condition for the game.
+# - Allow users to set a profile picture.
+# - Style the cards so that they look more like cards.
+# - Create admin users that are able to create, edit and delete cards, unlike normal players.
