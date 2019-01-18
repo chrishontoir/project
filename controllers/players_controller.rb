0966d7cc62c797e8653bbe239929ee0c1b08e1fb
user <user@users-MacBook-Pro.local>
2019-01-18 17:49:03 +0000

    Updated the add player function so that it assigns a Default Deck with 10 cards to a new player automatically.

--- controllers/players_controller.rb
+++ controllers/players_controller.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/reloader') if development?
 require_relative('../models/player.rb')
 require_relative('../models/deck.rb')
+require_relative('../models/card_deck.rb')
 also_reload('../models/*')
 
 get '/players' do
@@ -17,7 +18,18 @@ end
 post '/players' do
   player = Player.new(params)
   player.save
-  redirect to("/players")
+  deck = Deck.new({"name" => "Default Deck", "player_id" => player.id}).save()
+  card_deck1 = Card_Deck.new({"card_id" => 1, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck2 = Card_Deck.new({"card_id" => 2, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck3 = Card_Deck.new({"card_id" => 1, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck4 = Card_Deck.new({"card_id" => 2, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck5 = Card_Deck.new({"card_id" => 1, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck6 = Card_Deck.new({"card_id" => 2, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck7 = Card_Deck.new({"card_id" => 1, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck8 = Card_Deck.new({"card_id" => 2, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck9 = Card_Deck.new({"card_id" => 1, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  card_deck10 = Card_Deck.new({"card_id" => 2, "deck_id" => "#{deck}", "in_hand" => 0, "played" => 0}).save()
+  redirect to("/players/#{player.id}")
 end
 
 get '/players/:id' do
